41f93e044de9be27837a25919a1efba4f6f7ebc4
Nathaniel Mon Père <47852617+natevmp@users.noreply.github.com>
2024-07-01 11:50:36 +0100

    deploy documentation

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # MullerPlot
 
 [![Build Status](https://github.com/natevmp/MullerPlot.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/natevmp/MullerPlot.jl/actions/workflows/CI.yml?query=branch%3Amain)
+
+[![codecov.io](http://codecov.io/github/natevmp/MullerPlot.jl/coverage.svg?branch=main)](http://codecov.io/github/natevmp/MullerPlot.jl?branch=main)
--- src/MullerPlot.jl
+++ src/MullerPlot.jl
@@ -5,7 +5,7 @@ export mullerBounds, buildFamilyArray, variantBirthTimes
 """
     buildFamilyArray(parentVid_vid)
 
-Create a vector `vid_child_Vid`, indexed by the variant id's `vid`, that has as elements the children of each variant stored as a vector of id's. For example, `vid_child_Vid[5]` is a vector `vid_child` representing the children of variant with `vid=5`, where the elements are the `vid`'s of the respective children.
+Create a vector `vid_child_Vid`, indexed by the variant id's `vid`, that has as elements the children of each variant stored as a vector of id's. For example, `vid_child_Vid[5]` is a vector `vid_child` whose elements are the `vid`'s of the children of the variant with `vid=5`.
 
 `parentVid_vid`:    Vector indexed by the variant id's `vid`, where each element refers to that variant's parent `vid`. For example `parentVid_vid[5]` is the `vid` of the parent of the variant with `vid=5`.
 """
